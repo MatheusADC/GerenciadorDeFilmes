9d6b6340ffd2073b126ae23f6978995c962e5c3b
Matheus Amaral <161625293+MatheusADC@users.noreply.github.com>
2026-04-28 08:48:02 -0300

    ✨ feat: Implementação da Requisição de Login com Armazenamento de Token

--- src/app/core/services/user-api.ts
+++ src/app/core/services/user-api.ts
@@ -1,19 +1,27 @@
 import { HttpClient } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
 import { IUserTokenSuccessAuthResponse } from "../../shared/models/user-token-success-auth-response";
+import { IUserLoginSuccessResponse } from "../../shared/models/user-login-success-response";
+import { tap } from "rxjs";
+import { UserTokenStore } from "./user-token-store";
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserApi {
   private readonly _httpClient = inject(HttpClient);
+  private readonly _userTokenStore = inject(UserTokenStore);
 
   validateToken() {
     return this._httpClient.get<IUserTokenSuccessAuthResponse>('http://localhost:3000/users/validate-token');
   }
 
-  login() {
-
+  login(email: string, password: string) {
+    return this._httpClient.post<IUserLoginSuccessResponse>('http://localhost:3000/users/login', {
+      email,
+      password,
+    })
+    .pipe(tap((loginResponse) => this._userTokenStore.saveToken(loginResponse.token)));
   }
 
   register() {
