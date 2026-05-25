90103e7cc7f0faa41dde001fc7dea7527ea9d4f9
Matheus Amaral <161625293+MatheusADC@users.noreply.github.com>
2026-05-25 08:46:31 -0300

    ✨ feat: Implementação da limpeza do filtro

--- src/app/features/movies/components/movies-filter/movies-filter.ts
+++ src/app/features/movies/components/movies-filter/movies-filter.ts
@@ -10,4 +10,9 @@ import { FormsModule } from '@angular/forms';
 export class MoviesFilter {
   title = model('');
   category = model('');
+
+  clearFilter() {
+    this.title.set('');
+    this.category.set('');
+  }
 }
