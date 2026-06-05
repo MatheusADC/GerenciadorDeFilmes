ef6deb8a87d8edd89a6c23a41bef90de87416080
Matheus Amaral <161625293+MatheusADC@users.noreply.github.com>
2026-06-05 12:00:08 -0300

    ✨ feat: Implementação da Lógica de preenchimento das estrelas

--- src/app/features/movies/pages/movie-details/movie-details.ts
+++ src/app/features/movies/pages/movie-details/movie-details.ts
@@ -1,5 +1,5 @@
 import { DecimalPipe } from '@angular/common';
-import { Component, inject, input, linkedSignal, signal, WritableSignal } from '@angular/core';
+import { Component, computed, inject, input, linkedSignal, signal, WritableSignal } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { FavoritesApi } from '../../../../shared/services/favorites-api';
 import { MoviesApi } from '../../services/movies-api';
@@ -34,12 +34,24 @@ export class MovieDetails {
   isFavorite = signal(false);
   currentRating = signal<number | undefined>(undefined);
 
+  starsStatusFilled = computed(() => {
+    const rating = this.currentRating() ?? 0;
+
+    const boolArray = [0, 1, 2, 3, 4].map((index) => index < rating);
+
+    return boolArray;
+  });
+
   toggleFavorite() {
     this.isFavorite.update((value) => !value);
     console.log(`Filme agora é favorito: ${this.isFavorite()}`);
   }
 
   updateRating(newRating: number) {
-
+    if (newRating === this.currentRating()) {
+      this.currentRating.set(0);
+    } else {
+      this.currentRating.set(newRating);
+    }
   }
 }
